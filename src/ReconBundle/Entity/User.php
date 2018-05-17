c8671abaad50fcf8b4a5cb60516919969f3c17cf
JKoehren <koe.jimmy@gmail.com>
2018-05-17 16:11:34 +0200

    remove fos + finish index css

--- src/ReconBundle/Entity/User.php
+++ src/ReconBundle/Entity/User.php
@@ -2,7 +2,6 @@
 
 namespace ReconBundle\Entity;
 
-use FOS\UserBundle\Model\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -11,7 +10,7 @@ use Doctrine\ORM\Mapping as ORM;
  * @ORM\Table(name="user")
  * @ORM\Entity(repositoryClass="ReconBundle\Repository\UserRepository")
  */
-class User extends BaseUser
+class User
 {
     /**
      * @var int
@@ -91,6 +90,13 @@ class User extends BaseUser
      * @ORM\Column(name="prenom", type="string", length=255, nullable=true)
      */
     private $prenom;
+    
+     /**
+     * @var string
+     *
+     * @ORM\Column(name="email", type="string", length=255, nullable=true)
+     */
+    private $email;
     
     /**
     * @ORM\OneToMany(targetEntity="Situation", mappedBy="user")
@@ -352,6 +358,30 @@ class User extends BaseUser
     public function getPrenom()
     {
         return $this->prenom;
+    }
+    
+        /**
+     * Set prenom
+     *
+     * @param string $prenom
+     *
+     * @return User
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get prenom
+     *
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
     }
 }
 
