e524c9482b6f3466b1ae74594d5578603ca30af6
kioup <mftrookie@example.com>
2018-05-17 17:13:12 +0200

    Formulaire presque 80%

--- src/ReconBundle/Controller/UserController.php
+++ src/ReconBundle/Controller/UserController.php
@@ -1,7 +1,6 @@
 <?php
 
 namespace ReconBundle\Controller;
-namespace PN\CabinetMedicalBundle\Form; 
 use Symfony\Component\Form\AbstractType; 
 use Symfony\Component\Form\FormBuilderInterface; 
 use Symfony\Component\OptionsResolver\OptionsResolver; 
@@ -16,7 +15,36 @@ class UserController extends Controller
 {
     public function indexAction()
     {
-        
-        return $this->render('ReconBundle:Default:index.html.twig');
+        echo "prout";
+        //return $this->render('ReconBundle:Default:index.html.twig');
     }
+    
+    public function ajouterAction(Request $request) 
+        
+    {  
+        $user = new User();  
+        $form = $this->createForm(UserType::class, $user);  
+        if ($request->isMethod('POST')) {
+    // L'ordre ci-dessous place les données du formulaire dans l'entité $patient
+            $form->handleRequest($request); 
+        // Si le formulaire est valide (les valeurs entrées sont correctes)   
+            if ($form->isValid()) { 
+            // On persiste $patient    
+                $em = $this->getDoctrine()->getManager();        
+                $em->persist($user);    
+                $em->flush(); 
+                $request->getSession()->getFlashBag()->add('notice', 'Nouvel utilisateur enregistré.'); 
+            // On redirige vers la liste des utilisateur 
+
+                return $this->redirectToRoute('userlist', array('id' => $user->getId())); 
+            } 
+            
+        }  
+        
+        $message='';  
+        return $this->render('@Recon/User/ajouter.html.twig', 
+        array('form' => $form->createView(), 'message' => $message) 
+        ); 
+    } 
+
 }
--- src/ReconBundle/Form/UserType.php
+++ src/ReconBundle/Form/UserType.php
@@ -37,32 +37,4 @@ class UserType extends AbstractType
         return 'reconbundle_user';
     }
     
-    public function ajouterAction(Request $request) 
-        
-    {  
-        $user = new User();  
-        $form = $this->createForm(UserType::class, $user);  
-        if ($request->isMethod('POST')) {
-    // L'ordre ci-dessous place les données du formulaire dans l'entité $patient
-        $form->handleRequest($request); 
-    // Si le formulaire est valide (les valeurs entrées sont correctes)   
-            if ($form->isValid()) { 
-    // On persiste $patient    
-            $em = $this->getDoctrine()->getManager();        
-            $em->persist($user);    
-            $em->flush(); 
-            $request->getSession()->getFlashBag()->add('notice', 'Nouvel utilisateur enregistré.'); 
-    // On redirige vers la liste des utilisateur 
-       
-            return $this->redirectToRoute('userlist', array('id' => $user->getId())); 
-            } 
-            
-        }  
-        
-        $message='';  
-        return $this->render('@Recon/User/ajouter.html.twig', 
-        array('form' => $form->createView(), 'message' => $message) 
-        ); 
-    } 
-
 }
